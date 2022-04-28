68d5a94c5541ba32b3d036930173076cc79cf436
Mubashira M K <mubashiramk07@gmail.com>
2022-04-28 15:50:19 +0530

    Calender

--- src/Assets/newProjectData.js
+++ src/Assets/newProjectData.js
@@ -1,12 +1,18 @@
 export const newProjectdata = [
   {
-    img: "https://previews.123rf.com/images/vska/vska1606/vska160600078/58664054-encabezado-del-sitio-web-digital-abstracto-la-tecnolog%C3%ADa-de-fondo-banner.jpg",
+    id: 1,
     title: "App UX Design",
+    start: "2022-04-14T10:00:00",
+    end: "2022-04-14T12:00:00",
+    img: "https://previews.123rf.com/images/vska/vska1606/vska160600078/58664054-encabezado-del-sitio-web-digital-abstracto-la-tecnolog%C3%ADa-de-fondo-banner.jpg",
     text: "Food App UX design and make the right design direction for UI designer.",
   },
   {
-    img: "https://c8.alamy.com/comp/2AR80Y7/gears-on-an-elongated-background-website-header-technology-corporte-wallpaper-2AR80Y7.jpg",
+    id: 2,
     title: "Moution Graphics",
+    start: "2022-04-16T10:00:00",
+    end: "2022-04-20T12:00:00",
+    img: "https://c8.alamy.com/comp/2AR80Y7/gears-on-an-elongated-background-website-header-technology-corporte-wallpaper-2AR80Y7.jpg",
     text: "Food App UX design and make the right design direction for UI designer.",
   },
 ];
--- src/Pages/Calendar/index.js
+++ src/Pages/Calendar/index.js
@@ -1,3 +1,32 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { newProjectdata } from "../../Assets/newProjectData";
+
 export const Calendar = () => {
-  return "calender";
+  return (
+    <div className="ki">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next,today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        customButtons={{
+          new: {
+            text: "new",
+            click: () => console.log("new event"),
+          },
+        }}
+        events={newProjectdata}
+        eventColor="rgba(57, 84, 255, 1)"
+        nowIndicator
+        // dateClick={(e) => console.log(e.dateStr)}
+        // eventClick={(e) => console.log(e.event.id)}
+      />
+    </div>
+  );
 };
